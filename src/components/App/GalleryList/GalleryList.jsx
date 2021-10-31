95bb43a7debb16f9ab68ac4e4b3f3659cce2f80b
abdishakur rage <abdishakurrage06@gmail.com>
2021-10-31 15:22:55 -0500

    gallery list created

---
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem';
+
+function GalleryList({onGetGallery, galleryList, likeButton}) {
+
+
+    return (
+
+    );
+}
+
+
+export default GalleryList;
