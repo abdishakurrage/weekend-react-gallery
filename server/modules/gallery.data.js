17c2eebb9880fd3d1964a6bf73377b6b82a45328
abdishakur rage <abdishakurrage06@gmail.com>
2021-10-31 14:47:15 -0500

    added more pictures

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, title: 'goat', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, title: 'tiger', path: 'images/tiger.jpeg', description: 'Photo of a Royal Bengal Tiger.', likes: 0 },
+    { id: 3, title: 'lion', path: 'images/lion.jpeg', description: 'Photo of an African Lion.', likes: 0 }
 ];
 
 module.exports = galleryItems;
