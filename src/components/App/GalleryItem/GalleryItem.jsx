30dc70316f6aad5de45b03b9b4600d3a04634bba
abdishakur rage <abdishakurrage06@gmail.com>
2021-10-31 15:14:24 -0500

    gallery  item updated

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,37 @@
 import {useState} from 'react';
 
-function GalleryItem ({}) {
+function GalleryItem ({onGetGallery, galleryList, likeButton, image}) {
 
-return ()
+    let [showDescription, setShowDescription] = useState(true);
 
+    const handleClick = () => {
+        setShowDescription(!showDescription);
+    }
+
+    return (
+        <>
+            {
+                showDescription ? 
+                    <div key= {image.id}>
+                        <img src={image.path} 
+                            alt={image.title} 
+                            id={image.id}
+                            width="100" 
+                            height="100"
+                            onClick ={() => {handleClick()}}
+                        />
+                    </div>
+                    :
+                    <div 
+                        id={image.id}
+                        onClick ={() => {handleClick()}}
+                    >{image.description}
+                    </div>
+            }
+            <button onClick={() => {likeButton(image.id)}}>Like</button>
+            <h4>{image.likes} People like this</h4>
+        </>
+    );
 }
 
 export default GalleryItem;
